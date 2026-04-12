f0c996274f27d1a761e5e6cc49325d2d8ae580d2
Kashyap Solanki <kash.cdac@gmail.com>
2026-04-12 15:31:02 +0530

    form-component-implement

---
+++ src/app/sign-up/sign-up-form-model/sign-up-form-model.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-form-model',
+  imports: [],
+  templateUrl: './sign-up-form-model.html',
+  styleUrl: './sign-up-form-model.css',
+})
+export class SignUpFormModel {
+  // name: string;
+  // contactNo: string;
+  // contactNo: string;
+  // email: string;
+  // password: string;
+  // role: UserRole;
+}
